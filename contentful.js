09064c539db621b4f0e5522713c44e7cbfe2c2af
ela_lisondra <emrlisondra@gmail.com>
2021-07-04 20:05:50 +0100

    initial commit

---
+++ contentful.js
@@ -0,0 +1,7 @@
+const spaceId = "p7e2t7i7mzr0"
+const environmentId = "master"
+const accessToken = "XayahuBW0ZlzM1aIjcfxfGoIIYyjCa6URLK8-LebFsE"
+
+const url = `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${accessToken}`
+
+console.log(url)
